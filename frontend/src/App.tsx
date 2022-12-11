a04be806e6512faa62dd0be63198f9849f66b83d
Admin <valntn.mrl@gmail.com>
2022-12-11 14:31:45 +0100

    Fix code: Delete dead code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,34 +4,10 @@ import Home from './components/Home/Home';
 
 const logo: string = require("./logo.svg").default;
 
-
-
 function App() {
     return (
         <Home />
     );
-
-    /*
-    //const authContext = useContext(AuthContext);
-    return (
-        <div className="App" >
-            <header className="App-header" >
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code> src / App.tsx </code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-    */
 }
 
 export default App;
